2773657aaa916768d6be00a8082ee3c2c911c796
Avik-creator <avikm744@gmail.com>
2025-11-15 08:56:20 +0530

    feat: add CustomPage component for meme creation

---
+++ app/create/custom/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Meme from "@/components/meme"
+
+const CustomPage = () => {
+    return (
+        <div className="flex flex-col items-center md:mt-32 mt-12 gap-4 justify-center overflow-y-auto">
+            <Meme />
+        </div>
+    )
+}
+
+export default CustomPage
+
